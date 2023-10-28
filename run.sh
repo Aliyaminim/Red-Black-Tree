298f91a866c87b3a18b5256a8008d9a9265a2404
Minimullina Aliya <minimullina.ae@phystech.edu>
2023-10-28 10:25:23 +0300

    added unit tests

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf build
+mkdir -p build
+cd build/
+#cmake ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make
+cd ..
+#cd ./build/test
+#./test1
--- test/test1.cpp
+++ test/test1.cpp
@@ -4,12 +4,54 @@
 
 TEST(Test_lowerbound, Test1) {
     Trees::Search_RBTree<int> tree{}; 
-    int x = (rand() % 100) + 1;
+    int x = (rand() % 50) + 1;
+    int comp = x + (rand() % 5) + 1;
     for (int i = 0; i < 100; ++i) {
         int y = (rand() % 100) + 1;
-        tree.rb_insert(y);
-        assert((y != x) || (y != (x+1)));
+        if ((y < x) && (y > comp))   
+            tree.rb_insert(y);
     }
-    tree.rb_insert(x+2);
-    ASSERT_TRUE(tree.lower_bound(x)->key == (x+2));
+    tree.rb_insert(comp);
+    ASSERT_TRUE(tree.lower_bound(x)->key == comp);
 }
+
+TEST(Test_lowerbound, Test2) {
+    Trees::Search_RBTree<int> tree{}; 
+    int x = (rand() % 50) + 1;
+    int comp = x + (rand() % 5) + 1;
+    for (int i = 0; i < 100; ++i) {
+        int y = (rand() % 100) + 1;
+        if ((y < x) && (y > comp))   
+            tree.rb_insert(y);
+    }
+    tree.rb_insert(comp);
+    ASSERT_TRUE(tree.lower_bound(x)->key == comp);
+}
+
+TEST(Test_upperbound, Test1) {
+    Trees::Search_RBTree<int> tree{}; 
+    int x = (rand() % 50) + 1;
+    int comp = x + (rand() % 5) + 1;
+    for (int i = 0; i < 100; ++i) {
+        int y = (rand() % 100) + 1;
+        if ((y < x) && (y > comp))   
+            tree.rb_insert(y);
+    }
+    tree.rb_insert(comp);
+    tree.rb_insert(x);
+    ASSERT_TRUE(tree.upper_bound(x)->key == comp);
+}
+
+TEST(Test_upperbound, Test2) {
+    Trees::Search_RBTree<int> tree{}; 
+    int x = (rand() % 50) + 1;
+    int comp = x + (rand() % 5) + 1;
+    for (int i = 0; i < 100; ++i) {
+        int y = (rand() % 100) + 1;
+        if ((y < x) && (y > comp))   
+            tree.rb_insert(y);
+    }
+    tree.rb_insert(comp);
+    ASSERT_TRUE(tree.upper_bound(x)->key == comp);
+}
+
