45e232f37967e8a9b884d72dd0f60819833e30f1
Minimullina Aliya <minimullina.ae@phystech.edu>
2023-11-28 23:24:57 +0300

    added new tests

--- test/testing.sh
+++ test/testing.sh
@@ -3,7 +3,16 @@
 echo Testing has started!
 echo 
 echo "End-to-end tests:"
-for file in e2e/*.in; do
+for file in $(find . -name "test2*" ! -name "test2.in"); do
+    echo $file
+    echo "RBT: "
+    ../build/main_rbt < $file
+    echo "Standard Set: "
+    line=$(head -n 3 $file | tail -n 1)
+    echo "$line"
+    echo 
+done
+for file in $(find . -name "test*.in" -not -name "test2*"); do
     echo $file
     echo "RBT: "
     ../build/main_rbt < $file
