370917dbd2c21c8c1d0224e7ae69ecbd23c7cce5
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-02-11 14:08:00 +0300

    added lookup unit tests for tree

--- test/unit_tests/src/lookup.cpp
+++ test/unit_tests/src/lookup.cpp
@@ -57,11 +57,50 @@ TEST(Test_upperbound, Test2) {
     ASSERT_TRUE(tree.upper_bound(x)->key == comp);
 }
 
+TEST(Lookup, find) {
+    Trees::Search_RBTree<int> tree{}; 
+    for (int i = 0; i < 10; i++)
+        tree.insert(i);
+    for (int i = 0; i < 10; i++)
+        EXPECT_EQ(tree.find(i)->key, i);
+}
 
-//TEST(Lookup, find)
+TEST(Lookup, contains) {
+    Trees::Search_RBTree<int> tree{}; 
+    for (int i = 0; i < 10; i++)
+        tree.insert(i);
+    for (int i = 0; i < 10; i++)
+        EXPECT_TRUE(tree.contains(i));
+    
+    Trees::Search_RBTree<int> tree_empty{}; 
+    EXPECT_FALSE(tree_empty.contains(0));
+}
 
-//TEST(Lookup, contains)
+TEST(Lookup, nth_smallest) {
+    Trees::Search_RBTree<int> tree{}; 
+    for (int i = 1; i <= 10; i++)
+        tree.insert(i);
+    for (int i = 1; i <= 10; i++)
+        EXPECT_EQ(tree.select_ranked_elem(i), i);
 
-//TEST(Lookup, nth_smallest)
+    EXPECT_EQ(tree.select_ranked_elem(11), 10);
+}
 
-//TEST(Lookup, n_less_than)
+TEST(Lookup, n_less_than) {
+    Trees::Search_RBTree<int> tree{}; 
+    for (int i = 1; i < 10; i += 2)
+        tree.insert(i);
+    int kol = 1;
+    for (int i = 2; i < 11; i += 2, kol++) 
+        EXPECT_EQ(tree.key_rank(i), kol);
+
+    EXPECT_EQ(tree.key_rank(11), 5);
+}
+
+TEST(Lookup, range_query) {
+    Trees::Search_RBTree<double> tree{};
+    for (double i = 1; i < 10; i++)
+        tree.insert(i/2);
+    EXPECT_EQ(tree.range_query(1, 2), 3);
+    EXPECT_EQ(tree.range_query(1, 4), 7);
+}
